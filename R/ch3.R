eaa0519e9fa881e501e6bc1230295c83f5a22ac1
Damon Snyder <drsnyder@gmail.com>
2014-05-21 15:54:58 -0700

    checkpoint

--- R/ch3.R
+++ R/ch3.R
@@ -5,3 +5,4 @@ github.download = function(url) {
   return(fname)
 }
 custdata <- read.table(github.download("https://raw.githubusercontent.com/WinVector/zmPDSwR/master/Custdata/custdata.tsv"),header=T,sep='\t')
+custdata <- read.table("https://raw.githubusercontent.com/WinVector/zmPDSwR/master/Custdata/custdata.tsv",header=T,sep='\t')
